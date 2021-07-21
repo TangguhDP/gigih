2b71f7208d8842a6bc9d987ce17d0fcacd61567f
Tangguh Pramono <tangguhmyname@gmail.com>
2021-07-21 19:18:25 +0700

    Adding Day 8 Exercise

--- Day3n4/HandsOn/giphy-app/src/pages/Search.js
+++ Day3n4/HandsOn/giphy-app/src/pages/Search.js
@@ -1,51 +1,40 @@
-import React, { Component } from "react";
-import GiphyWithSearch from "../components/GiphyWithSearch";
+import React, { useState } from "react";
+import Gif from "../components/Gif";
 
-export default class Search extends Component {
-  state = {
-    search: "",
-    data: [],
-  };
+export default function Search() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
 
-  getSearch = () => {
+  const getSearch = () => {
     fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_KEY}&q=${this.state.search}&limit=6`
+      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_KEY}&q=${search}&limit=6`
     )
       .then((response) => response.json())
       .then((response) => {
-        this.setState({ data: response.data });
+        setData(response.data);
       });
   };
-
-  render() {
-    return (
-      <div className="wrapper">
-          <input
-            value={this.state.search}
-            onChange={(e) => {
-              this.setState({ search: e.target.value });
-            }}
-            className="input-text"
-            type="text"
-            name="search"
-          />
-          <button
-            onClick={() => this.getSearch()}
-            className="search-button"
-            type="button"
-          >
-            Search
-          </button>
-        {this.state.data?.map((gif, i) => {
-          return (
-            <GiphyWithSearch
-              key={i}
-              title={gif.title}
-              url={gif.images.original.url}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+  return (
+    <div className="wrapper">
+      <input
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        className="input-text"
+        type="text"
+        name="search"
+      />
+      <button
+        onClick={() => getSearch()}
+        className="search-button"
+        type="button"
+      >
+        Search
+      </button>
+      {data?.map((gif, i) => {
+        return <Gif key={i} title={gif.title} url={gif.images.original.url} />;
+      })}
+    </div>
+  );
 }
